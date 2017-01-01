aed62bfb46ee5f0ff05e0148b818f52b09d0d54a
Nathan Duarte <nduartech101@gmail.com>
2017-01-01 03:28:06 -0500

    Practicing string manipulation in python

--- basic/string1.py
+++ basic/string1.py
@@ -25,7 +25,9 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+  if count<10:
+    return 'Number of donuts: ' + str(count)
+  return 'Number of donuts: many'
 
 
 # B. both_ends
@@ -35,7 +37,9 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+  if len(s)<2:
+    return ''
+  return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -49,7 +53,7 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  return s[:1] + s[1:].replace(s[:1],'*')
 
 
 # D. MixUp
@@ -61,7 +65,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
