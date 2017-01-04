6290615216b95f8cf9bfc5509766777a04f5a3c8
Nathan Duarte <nduartech101@gmail.com>
2017-01-04 07:10:52 -0500

    Url parsing and more file manipulation using urllib in python.

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -10,6 +10,7 @@ import os
 import re
 import sys
 import urllib
+import commands
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
@@ -18,13 +19,30 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
+def sort_method(url):
+  match = re.search(r'-\S+-(\S+)\.jpg', url)
+  if match:
+    return match.group(1)
+  else:
+    return url
 
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
-  # +++your code here+++
+  ufile = urllib.urlopen(filename)
+  baseurl = ufile.geturl()
+  match = re.search(r'_(\S+)', baseurl)
+  baseurl = match.group(1)
+  text = ufile.read()
+  puzzle_urls = re.findall(r'GET ([\/\S]+puzzle[\/\S]+\.jpg) HTTP', text)
+  complete_urls = []
+  for p in puzzle_urls:
+    if (baseurl+p) not in complete_urls:
+      complete_urls.append("https://"+baseurl+p)
+  complete_urls = sorted(complete_urls, key=sort_method)
+  return complete_urls
   
 
 def download_images(img_urls, dest_dir):
@@ -35,8 +53,21 @@ def download_images(img_urls, dest_dir):
   with an img tag to show each local image file.
   Creates the directory if necessary.
   """
-  # +++your code here+++
-  
+  if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+  index = file(os.path.join(dest_dir, 'index.html'), 'w')
+  index.write('<html><body>\n')
+  abs_path = os.path.abspath(dest_dir)
+  counter = 0
+  for i in img_urls:
+    local_name = "img%d" % counter
+    urllib.urlretrieve(i, os.path.join(abs_path, local_name))
+    counter += 1
+    print "Retrieving...", i
+    index.write('<img src="%s">' % (local_name,))
+  index.write('\n</body></html>\n')
+  index.close()
+
 
 def main():
   args = sys.argv[1:]
@@ -55,7 +86,8 @@ def main():
   if todir:
     download_images(img_urls, todir)
   else:
-    print '\n'.join(img_urls)
+    for g in img_urls:
+      print "\n" + g
 
 if __name__ == '__main__':
   main()
